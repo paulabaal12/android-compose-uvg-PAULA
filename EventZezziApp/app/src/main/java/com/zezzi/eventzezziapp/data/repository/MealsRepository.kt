d11a71b068554c663ddb19da945fbfd54f1a4c0b
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-10-05 16:54:40 -0600

    Cmabios

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
@@ -5,21 +5,25 @@ import com.zezzi.eventzezziapp.data.networking.response.MealsCategoriesResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MealsRepository(private val webService: MealsWebService = MealsWebService()) {
-    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
-        return webService.getMeals().enqueue(object : Callback<MealsCategoriesResponse> {
-            override fun onResponse(
-                call: Call<MealsCategoriesResponse>,
-                response: Response<MealsCategoriesResponse>
-            ) {
-                if (response.isSuccessful)
-                    successCallback(response.body())
-            }
 
-            override fun onFailure(call: Call<MealsCategoriesResponse>, t: Throwable) {
-                // TODO treat failure
+    suspend fun getMeals(): MealsCategoriesResponse? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = webService.getMeals()
+                if (response.isSuccessful) {
+                    return@withContext response.body()
+                } else {
+
+                    return@withContext null
+                }
+            } catch (e: Exception) {
+
+                return@withContext null
             }
-        })
+        }
     }
 }
